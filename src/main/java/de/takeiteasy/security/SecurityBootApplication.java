358e17f6e3a2923f3c193bca67345e9d6411e6e2
ranjit <ranjit.patel@billpay.de>
2019-06-25 22:46:39 +0200

    added support for spring security

---
+++ src/main/java/de/takeiteasy/security/SecurityBootApplication.java
@@ -0,0 +1,13 @@
+package de.takeiteasy.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBootApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(SecurityBootApplication.class,args);
+    }
+
+}
